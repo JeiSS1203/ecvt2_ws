bc4fad2ea1ae7bda44e6affe67c923325d65b27d
JeiSS1203 <wlsrhkd1203@gmail.com>
2026-03-16 16:40:49 +0900

    PFRC 뺀 버전

--- ecvt2_sim/launch/test_upper_bringup.launch.py
+++ ecvt2_sim/launch/test_upper_bringup.launch.py
@@ -8,24 +8,43 @@ def generate_launch_description():
         name='sim_ecvt_upper_node',
         output='screen'
     )
-
-    dynamics_node = Node(
+    
+    planner_node = Node(
         package='ecvt2_controller',
-        executable='live_upper_dynamics_node',
-        name='live_upper_dynamics_node',
+        executable='vp_sto_global_planner_node',
+        name='vp_sto_global_planner_node',
         output='screen',
         parameters=[{
-            'urdf_path': '/home/jin/harco/ecvt2_ws/forestry_robot_mjcf/xml/ecvt_v2_upper.urdf',
             'joint_state_topic': '/joint_states',
-            'state_joints': ['UPJ1','UPJ2','UPJ3','UPJ4','UPJ5','UPJ6','TOOLJ1'],
-            'actuated_joints': ['UPJ1','UPJ2','UPJ3','UPJ4'],
+            'actuated_joints': ['UPJ1','UPJ2','UPJ3','UPJ4','TOOLJ1'],
             'passive_joints': ['UPJ5','UPJ6'],
-            'print_full_matrix': False,
-            'log_every_n': 20,
+            'goal_actuated': [0.2, 0.5, -0.8, 0.6, 0.0],
+            'velocity_limits': [0.2, 0.2, 0.2, 0.2, 0.2],
+            'acceleration_limits': [0.5, 0.5, 0.5, 0.5, 0.5],
+            'n_via': 5,
+            'n_eval': 100,
+            'population': 40,
+            'max_iterations': 20
         }]
     )
+    # dynamics_node = Node(
+    #     package='ecvt2_controller',
+    #     executable='live_upper_dynamics_node',
+    #     name='live_upper_dynamics_node',
+    #     output='screen',
+    #     parameters=[{
+    #         'urdf_path': '/home/jin/harco/ecvt2_ws/forestry_robot_mjcf/xml/ecvt_v2_upper.urdf',
+    #         'joint_state_topic': '/joint_states',
+    #         'state_joints': ['UPJ1','UPJ2','UPJ3','UPJ4','UPJ5','UPJ6','TOOLJ1'],
+    #         'actuated_joints': ['UPJ1','UPJ2','UPJ3','UPJ4'],
+    #         'passive_joints': ['UPJ5','UPJ6'],
+    #         'print_full_matrix': False,
+    #         'log_every_n': 20,
+    #     }]
+    # )
 
     return LaunchDescription([
         upper_node,
-        dynamics_node,
+        # dynamics_node,
+        planner_node
     ])
